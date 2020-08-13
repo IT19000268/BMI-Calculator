0eda1540d00ee4d481201896727e32536e8491ad
kithmini1998 <desilvakithmini@gmail.com>
2020-08-13 17:17:57 +0530

    OverWeight Page Added

--- app/src/main/java/com/example/bmicalculator/BMI_Classification.java
+++ app/src/main/java/com/example/bmicalculator/BMI_Classification.java
@@ -2,13 +2,32 @@ package com.example.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BMI_Classification extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b_m_i__classification);
+        OnClickButtonListner();
+
+    }
+    public void OnClickButtonListner(){
+        button=(Button)findViewById(R.id.button15);
+        button.setOnClickListener(
+                new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent=new Intent(".OverWeight");
+                        startActivity(intent);
+                    }
+                }
+
+        );
     }
 }
