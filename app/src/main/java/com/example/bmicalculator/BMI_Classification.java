614cdf144808c333f155d5606d4c9674e78892ba
IT19000268 <samindimihisarani@gmail.com>
2020-08-13 16:04:16 +0530

    initial commit

---
+++ app/src/main/java/com/example/bmicalculator/BMI_Classification.java
@@ -0,0 +1,14 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BMI_Classification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b_m_i__classification);
+    }
+}
