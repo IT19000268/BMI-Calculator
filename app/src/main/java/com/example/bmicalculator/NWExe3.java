140dc3f0fa0b9ffd6621970bb9b41b78de27debe
Damika Sulaksen <damikadesilva@gmail.com>
2020-09-24 12:36:41 +0530

    First commit New

---
+++ app/src/main/java/com/example/bmicalculator/NWExe3.java
@@ -0,0 +1,14 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NWExe3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_n_w_exe3);
+    }
+}
---
+++ app/src/main/java/com/example/bmicalculator/NWeightDiet2.java
@@ -0,0 +1,14 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NWeightDiet2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_n_weight_diet2);
+    }
+}
